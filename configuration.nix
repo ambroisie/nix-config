81a08e7d98590f544d92f227f6095b058c32259e
Bruno BELANYI <bruno@belanyi.fr>
2021-02-01 15:34:48 +0100

    configuration: allow unfree packages

--- configuration.nix
+++ configuration.nix
@@ -107,4 +107,6 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "20.09"; # Did you read the comment?
+
+  nixpkgs.config.allowUnfree = true; # Because I don't care *that* much.
 }
