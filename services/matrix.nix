6b1de02ea3bfbf5eb321ec3aece91cd109c20080
Bruno BELANYI <bruno@belanyi.fr>
2021-02-04 17:06:11 +0100

    services: matrix: configure DB on launch

--- services/matrix.nix
+++ services/matrix.nix
@@ -24,6 +24,13 @@ in
     services.postgresql = {
       enable = true;
       package = pkgs.postgresql_12;
+      initialScript = pkgs.writeText "synapse-init.sql" ''
+        CREATE ROLE "matrix-synapse" WITH LOGIN PASSWORD 'synapse';
+        CREATE DATABASE "matrix-synapse" WITH OWNER "matrix-synapse"
+          TEMPLATE template0
+          LC_COLLATE = "C"
+          LC_CTYPE = "C";
+      '';
     };
 
     services.postgresqlBackup = {
