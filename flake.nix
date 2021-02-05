4d68a706a34f0d82792e08a7b0d89c9c91669709
Bruno BELANYI <bruno@belanyi.fr>
2021-02-05 16:43:30 +0100

    flake: add NUR overlay

--- flake.nix
+++ flake.nix
@@ -2,9 +2,10 @@
   description = "Nixos configuration with flakes";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nur.url = "github:nix-community/NUR";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, nur }: {
     nixosConfigurations.porthos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules =
@@ -17,6 +18,7 @@
               then self.rev
               else throw "Refusing to build from a dirty Git tree!";
           })
+          { nixpkgs.overlays = [ nur.overlay ]; }
           ./configuration.nix
         ];
     };
