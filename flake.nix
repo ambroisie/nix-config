34548c28ec2f9b019b002b7d9c407be6e79af203
Bruno BELANYI <bruno@belanyi.fr>
2021-02-05 16:25:39 +0100

    flake: init configuration

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Nixos configuration with flakes";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.porthos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        [
+          ({ pkgs, ... }: {
+            # Let 'nixos-version --json' know about the Git revision
+            # of this flake.
+            system.configurationRevision =
+              if self ? rev
+              then self.rev
+              else throw "Refusing to build from a dirty Git tree!";
+          })
+          ./configuration.nix
+        ];
+    };
+  };
+}
