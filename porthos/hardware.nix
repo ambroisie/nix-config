5e06025d67d7ca688be9c2033a3d641c920283f9
Bruno BELANYI <bruno@belanyi.fr>
2021-02-07 10:44:53 +0000

    porthos: hardware: clean-up style

--- porthos/hardware.nix
+++ porthos/hardware.nix
@@ -2,19 +2,18 @@
 { lib, modulesPath, ... }:
 
 {
-  imports =
-    [
-      (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
-  fileSystems."/" =
-    {
-      device = "/dev/disk/by-uuid/d89efc61-6b03-4190-b488-301c919e2431";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/d89efc61-6b03-4190-b488-301c919e2431";
+    fsType = "ext4";
+  };
 
-  swapDevices =
-    [{ device = "/dev/disk/by-uuid/1a261204-2e78-496f-8a8d-d29bfa770306"; }];
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/1a261204-2e78-496f-8a8d-d29bfa770306"; }
+  ];
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
 }
