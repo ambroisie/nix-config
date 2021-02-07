4f0a66c80e1d2dc476bc3fa70c3222e5d2238874
Bruno BELANYI <bruno@belanyi.fr>
2021-02-07 10:42:08 +0000

    porthos: boot: clean-up style

--- porthos/boot.nix
+++ porthos/boot.nix
@@ -2,14 +2,22 @@
 { ... }:
 
 {
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.device = "/dev/sda";
+  boot = {
+    # Use the GRUB 2 boot loader.
+    loader.grub = {
+      enable = true;
+      version = 2;
+      # Define on which hard drive you want to install Grub.
+      device = "/dev/sda";
+    };
 
-  boot.initrd.availableKernelModules = [ "uhci_hcd" "ahci" "usbhid" ];
-  boot.initrd.kernelModules = [ "dm-snapshot" ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
+    initrd = {
+      availableKernelModules = [ "uhci_hcd" "ahci" "usbhid" ];
+      kernelModules = [ "dm-snapshot" ];
+    };
+
+    kernelModules = [ "kvm-intel" ];
+
+    extraModulePackages = [ ];
+  };
 }
