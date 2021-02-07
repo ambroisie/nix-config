4ceb0f75525362ed813a6e0e50e839659a7f22a2
Bruno BELANYI <bruno@belanyi.fr>
2021-02-07 10:46:22 +0000

    porthos: networking: clean-up style

--- porthos/networking.nix
+++ porthos/networking.nix
@@ -2,26 +2,31 @@
 { ... }:
 
 {
-  networking.hostName = "porthos"; # Define your hostname.
-  networking.domain = "test.belanyi.fr"; # Define your domain.
+  networking = {
+    hostName = "porthos"; # Define your hostname.
+    domain = "test.belanyi.fr"; # Define your domain.
 
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.bond0.useDHCP = true;
-  networking.interfaces.bonding_masters.useDHCP = true;
-  networking.interfaces.dummy0.useDHCP = true;
-  networking.interfaces.erspan0.useDHCP = true;
-  networking.interfaces.eth0.useDHCP = true;
-  networking.interfaces.eth1.useDHCP = true;
-  networking.interfaces.gre0.useDHCP = true;
-  networking.interfaces.gretap0.useDHCP = true;
-  networking.interfaces.ifb0.useDHCP = true;
-  networking.interfaces.ifb1.useDHCP = true;
-  networking.interfaces.ip6tnl0.useDHCP = true;
-  networking.interfaces.sit0.useDHCP = true;
-  networking.interfaces.teql0.useDHCP = true;
-  networking.interfaces.tunl0.useDHCP = true;
+    # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+    # Per-interface useDHCP will be mandatory in the future, so this generated config
+    # replicates the default behaviour.
+    useDHCP = false;
+
+    interfaces = {
+      bond0.useDHCP = true;
+      bonding_masters.useDHCP = true;
+      dummy0.useDHCP = true;
+      erspan0.useDHCP = true;
+      eth0.useDHCP = true;
+      eth1.useDHCP = true;
+      gre0.useDHCP = true;
+      gretap0.useDHCP = true;
+      ifb0.useDHCP = true;
+      ifb1.useDHCP = true;
+      ip6tnl0.useDHCP = true;
+      sit0.useDHCP = true;
+      teql0.useDHCP = true;
+      tunl0.useDHCP = true;
+    };
+  };
 }
