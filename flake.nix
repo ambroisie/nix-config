c0feb8a46247c996bbd86330ae204622c9d440af
Bruno BELANYI <bruno@belanyi.fr>
2021-02-18 21:27:08 +0000

    flake: add home-manager

--- flake.nix
+++ flake.nix
@@ -1,11 +1,13 @@
 {
   description = "Nixos configuration with flakes";
   inputs = {
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nur.url = "github:nix-community/NUR";
   };
 
-  outputs = { self, nixpkgs, nur }:
+  outputs = { self, nixpkgs, nur, home-manager }:
     let
       buildHost = name: system: nixpkgs.lib.nixosSystem {
         inherit system;
@@ -20,6 +22,13 @@
           })
           { nixpkgs.overlays = [ nur.overlay ]; }
           (./. + "/${name}.nix")
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.users.ambroisie = import ./home;
+            # Nix Flakes compatibility
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+          }
         ];
       };
     in
