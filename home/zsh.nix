1daff408912c5566b6867c5d28b03fe0dfa9ee3d
Bruno BELANYI <bruno@belanyi.fr>
2021-02-19 19:16:08 +0000

    home: zsh: add more customisation
    This will make it closer to my usual shell setup in Arch.

--- home/zsh.nix
+++ home/zsh.nix
@@ -27,6 +27,21 @@
 
     # Modal editing is life, but CLI benefits from emacs gymnastics
     defaultKeymap = "emacs";
+
+    initExtra = ''
+      # Show an error when a globbing expansion doesn't find any match
+      setopt nomatch
+      # List on ambiguous completion and Insert first match immediately
+      setopt autolist menucomplete
+      # Use pushd when cd-ing around
+      setopt autopushd pushdminus pushdsilent
+      # Use single quotes in string without the weird escape tricks
+      setopt rcquotes
+      # Single word commands can resume an existing job
+      setopt autoresume
+      # Those options aren't wanted
+      unsetopt beep extendedglob notify
+    '';
   };
 
   # Fuzzy-wuzzy
