bd03e08709420addb5d9765e4cb25ce0db63c7d0
Bruno BELANYI <bruno@belanyi.fr>
2021-02-20 13:46:58 +0000

    home: pager: add less env variables

--- home/pager.nix
+++ home/pager.nix
@@ -1,4 +1,11 @@
 { ... }:
 {
   programs.lesspipe.enable = true;
+
+  home.sessionVariables = {
+    # My default pager
+    PAGER = "less";
+    # Clear the screen on start and exit
+    LESS = "-R -+X -c";
+  };
 }
