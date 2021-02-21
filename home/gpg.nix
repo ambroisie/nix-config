a65b7202b1c2e8ba55385c0d4704cfb50756f970
Bruno BELANYI <bruno@belanyi.fr>
2021-02-21 18:53:14 +0000

    home: gpg: configure pinentry

--- home/gpg.nix
+++ home/gpg.nix
@@ -7,6 +7,7 @@
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true; # One agent to rule them all
+    pinentryFlavor = "curses";
     extraConfig = ''
       allow-loopback-pinentry
     '';
