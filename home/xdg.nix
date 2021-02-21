f589b8d5d34e0bae26d288e33c74922549a39165
Bruno BELANYI <bruno@belanyi.fr>
2021-02-21 19:32:54 +0000

    home: xdg: configure user directories

--- home/xdg.nix
+++ home/xdg.nix
@@ -8,5 +8,18 @@
     mimeApps = {
       enable = true;
     };
+    # User directories
+    userDirs = {
+      enable = true;
+      # I want them lowercased
+      desktop = "\$HOME/desktop";
+      documents = "\$HOME/documents";
+      download = "\$HOME/downloads";
+      music = "\$HOME/music";
+      pictures = "\$HOME/pictures";
+      publicShare = "\$HOME/public";
+      templates = "\$HOME/templates";
+      videos = "\$HOME/videos";
+    };
   };
 }
