828c1663da23bb0451ee622ed6cdb15a096403bf
Bruno BELANYI <bruno@belanyi.fr>
2021-02-23 14:30:50 +0000

    home: vim: add plugins
    Pretty much every one that was in my dotfiles

--- home/vim/default.nix
+++ home/vim/default.nix
@@ -8,7 +8,36 @@
     vimdiffAlias = true;
 
     plugins = with pkgs.vimPlugins; [
+      # Theming
+      lightline-vim # Fancy status bar
+      onedark-vim # Nice dark theme
+
+      # tpope essentials
+      vim-commentary # Easy comments
+      vim-eunuch # UNIX integrations
+      vim-git # Sane git syntax files
+      vim-repeat # Enanche '.' for plugins
+      vim-rsi # Readline mappings
+      vim-surround # Deal with pairs
+      vim-unimpaired # Some ex command mappings
+      vim-vinegar # Better netrw
+
+      # Languages
+      rust-vim
+      vim-beancount
+      vim-jsonnet
       vim-nix
+      vim-pandoc
+      vim-pandoc-syntax
+      vim-toml
+
+      # General enhancements
+      fastfold # Better folding
+      vim-qf # Better quick-fix list
+
+      # LSP and linting
+      ale # Asynchronous Linting Engine
+      lightline-ale # Status bar integration
     ];
   };
 }
