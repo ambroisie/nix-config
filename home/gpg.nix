58c9b6072205192497a56ca17a67c3fcd0a1a529
Bruno BELANYI <bruno@belanyi.fr>
2021-03-13 00:01:29 +0000

    home: gpg: make it enable-able

--- home/gpg.nix
+++ home/gpg.nix
@@ -1,15 +1,24 @@
-{ ... }:
+{ config, lib, ... }:
+let
+  cfg = config.my.home.gpg;
+in
 {
-  programs.gpg = {
-    enable = true;
+  options.my.home.gpg = with lib.my; {
+    enable = mkDisableOption "gpg configuration";
   };
 
-  services.gpg-agent = {
-    enable = true;
-    enableSshSupport = true; # One agent to rule them all
-    pinentryFlavor = "tty";
-    extraConfig = ''
-      allow-loopback-pinentry
-    '';
+  config = lib.mkIf cfg.enable {
+    programs.gpg = {
+      enable = true;
+    };
+
+    services.gpg-agent = {
+      enable = true;
+      enableSshSupport = true; # One agent to rule them all
+      pinentryFlavor = "tty";
+      extraConfig = ''
+        allow-loopback-pinentry
+      '';
+    };
   };
 }
