37038369ff3f80afda6ede94264e37d525c24d41
Bruno BELANYI <bruno@belanyi.fr>
2021-03-13 00:02:12 +0000

    home: packages: make it enable-able

--- home/packages.nix
+++ home/packages.nix
@@ -1,6 +1,13 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.my.home.packages;
+in
 {
-  home.packages = with pkgs; [
+  options.my.home.packages = with lib.my; {
+    enable = mkDisableOption "user packages";
+  };
+
+  config.home.packages = with pkgs; lib.mkIf cfg.enable [
     # Git related
     gitAndTools.git-absorb
     gitAndTools.git-revise
