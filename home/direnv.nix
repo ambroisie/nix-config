130adae9c608100165d179266cef6520fe6bb233
Bruno BELANYI <bruno@belanyi.fr>
2021-03-13 00:01:00 +0000

    home: direnv: make it enable-able

--- home/direnv.nix
+++ home/direnv.nix
@@ -1,6 +1,13 @@
-{ ... }:
+{ config, lib, ... }:
+let
+  cfg = config.my.home.direnv;
+in
 {
-  programs.direnv = {
+  options.my.home.direnv = with lib.my; {
+    enable = mkDisableOption "direnv configuration";
+  };
+
+  config.programs.direnv = lib.mkIf cfg.enable {
     enable = true;
     # A better `use_nix`
     enableNixDirenvIntegration = true;
