af317ac156f882e5d2ef0370a48fb0345d7ce008
Bruno BELANYI <bruno@belanyi.fr>
2021-04-03 17:12:34 +0000

    lib: attrs: document functions

--- lib/attrs.nix
+++ lib/attrs.nix
@@ -1,9 +1,21 @@
 { lib, ... }:
 let
-  inherit (lib) filterAttrs mapAttrs';
+  inherit (lib) filterAttrs listToAttrs mapAttrs';
 in
 {
+  # Filter a generated set of attrs using a predicate function.
+  #
+  # mapFilterAttrs ::
+  #   (name -> value -> bool)
+  #   (name -> value -> { name = any; value = any; })
+  #   attrs
   mapFilterAttrs = pred: f: attrs: filterAttrs pred (mapAttrs' f attrs);
 
+  # Generate an attribute set by mapping a function over a list of values.
+  #
+  # genAttrs' ::
+  #   [ values ]
+  #   (value -> { name = any; value = any; })
+  #   attrs
   genAttrs' = values: f: listToAttrs (map f values);
 }
