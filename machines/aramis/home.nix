3d51af4f5499ea38d184d55921892f713fc26f59
Bruno BELANYI <bruno@belanyi.fr>
2021-04-05 21:01:28 +0000

    machines: aramis: home: enable X & i3

--- machines/aramis/home.nix
+++ machines/aramis/home.nix
@@ -1,4 +1,14 @@
 { ... }:
 {
-  # To be filled out
+  my.home = {
+    # i3 settings
+    wm.windowManager = "i3";
+    # Keyboard settings
+    x.enable = true;
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  # Enable i3
+  services.xserver.windowManager.i3.enable = true;
 }
