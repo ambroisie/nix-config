35b8de02c2bc768103175073169713bfc0cedcc7
Bruno BELANYI <bruno@belanyi.fr>
2021-05-05 12:47:04 +0200

    modules: add bluetooth

---
+++ modules/bluetooth.nix
@@ -0,0 +1,38 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.my.modules.bluetooth;
+in
+{
+  options.my.modules.bluetooth = with lib; {
+    enable = mkEnableOption "wireless configuration";
+
+    enableHeadsetIntegration = my.mkDisableOption "A2DP sink configuration";
+
+    loadExtraCodecs = my.mkDisableOption "extra audio codecs";
+  };
+
+  config = lib.mkIf cfg.enable (lib.mkMerge [
+    # Enable bluetooth devices and GUI to connect to them
+    {
+      hardware.bluetooth.enable = true;
+      services.blueman.enable = true;
+    }
+
+    # Support for additional bluetooth codecs
+    (lib.mkIf cfg.loadExtraCodecs {
+      hardware.pulseaudio = {
+        extraModules = [ pkgs.pulseaudio-modules-bt ];
+        package = pkgs.pulseaudioFull;
+      };
+    })
+
+    # Support for A2DP audio profile
+    (lib.mkIf cfg.enableHeadsetIntegration {
+      hardware.bluetooth.settings = {
+        General = {
+          Enable = "Source,Sink,Media,Socket";
+        };
+      };
+    })
+  ]);
+}
