226b8f7b6aeaaae177fc0908d9f3b32fda9ca0d2
Bruno BELANYI <bruno@belanyi.fr>
2021-05-09 17:45:05 +0200

    home: wm: i3bar: remove gammastep block
    It doesn't seem to be working well with my setup...

--- home/wm/i3bar.nix
+++ home/wm/i3bar.nix
@@ -34,12 +34,6 @@ in
               block = "net";
               format = "{ssid} {ip} {signal_strength}";
             }
-            (lib.optionalAttrs (config.my.home.gammastep.enable) {
-              block = "hueshift";
-              hue_shifter = "gammastep";
-              step = 100;
-              click_temp = config.my.home.gammastep.temperature.day;
-            })
             {
               block = "battery";
               format = "{percentage}% ({time})";
