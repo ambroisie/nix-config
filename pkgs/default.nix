4d0ab2e5a6e8978a857f2fd9f5a66aeb6ef5be5d
Bruno BELANYI <bruno@belanyi.fr>
2021-05-11 18:46:43 +0200

    pkgs: add ff2mpv-go

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,6 +4,8 @@ rec {
 
   diff-flake = pkgs.callPackage ./diff-flake { };
 
+  ff2mpv-go = pkgs.callPackage ./ff2mpv-go { };
+
   havm = pkgs.callPackage ./havm { };
 
   i3-get-window-criteria = pkgs.callPackage ./i3-get-window-criteria { };
