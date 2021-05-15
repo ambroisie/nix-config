4fcad6026a3e73d5a9a9c70fb3424818012315a4
Bruno BELANYI <bruno@belanyi.fr>
2021-05-15 20:25:09 +0200

    modules: nix: pin flake registry to common inputs

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,9 +1,17 @@
 # Nix related settings
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   nix = {
     package = pkgs.nixFlakes;
+    registry = {
+      # Allow me to use my custom package using `nix run self#pkg`
+      self.flake = inputs.self;
+      # Do not follow master, use pinned revision instead
+      nixpkgs.flake = inputs.nixpkgs;
+      # Add NUR to run some packages that are only present there
+      nur.flake = inputs.nur;
+    };
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
