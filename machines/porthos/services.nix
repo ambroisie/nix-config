a8514dcdf144b138b49ecd9571bf1977df1ed145
Bruno BELANYI <bruno@belanyi.fr>
2021-08-24 22:30:44 +0200

    modules: services: nginx: overhaul modularity
    This should be all that's needed for almost all my services.

--- machines/porthos/services.nix
+++ machines/porthos/services.nix
@@ -90,6 +90,9 @@ in
       enable = true;
       password = my.secrets.nextcloud.password;
     };
+    nginx = {
+      enable = true; # FIXME: remove this when done migrating
+    };
     # The whole *arr software suite
     pirate.enable = true;
     # Podcast automatic downloader
