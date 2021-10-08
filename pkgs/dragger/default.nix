0aa2f59530da1634b98f2130153e15aa5e2273bf
Bruno BELANYI <bruno@belanyi.fr>
2021-10-08 19:39:43 +0200

    pkgs: dragger: change platforms to linux
    I haven't tested it on darwin, and it just so happens that some
    dependency is broken on that system (breaking `nix flake check`)...
    
    Will revisit this later in case I ever happen to use a darwin system.

--- pkgs/dragger/default.nix
+++ pkgs/dragger/default.nix
@@ -1,4 +1,4 @@
-{ lib, fetchFromGitHub, qt5 }:
+{ lib, fetchFromGitHub, qt5, stdenv }:
 qt5.mkDerivation rec {
   pname = "dragger";
   version = "0.1.0";
@@ -24,6 +24,6 @@ qt5.mkDerivation rec {
     homepage = "https://gitea.belanyi.fr/ambroisie/dragger";
     license = licenses.mit;
     maintainers = [ ambroisie ];
-    platforms = platforms.all;
+    platforms = platforms.linux;
   };
 }
