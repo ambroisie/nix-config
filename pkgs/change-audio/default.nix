ce0c634a052ae5c7ce1621c3e0437300babd9890
Bruno BELANYI <bruno@belanyi.fr>
2021-11-12 09:49:00 +0100

    pkgs: change-audio: 0.1.1 -> 0.1.2
    Show if volume is currently muted.

--- pkgs/change-audio/default.nix
+++ pkgs/change-audio/default.nix
@@ -1,7 +1,7 @@
 { lib, libnotify, makeWrapper, pamixer, shellcheck, stdenvNoCC }:
 stdenvNoCC.mkDerivation rec {
   pname = "change-audio";
-  version = "0.1.1";
+  version = "0.1.2";
 
   src = ./change-audio;
 
