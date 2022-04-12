b6ca6b28b91db6ee9c91c6d2a37c2a907ac6e0e3
Bruno BELANYI <bruno@belanyi.fr>
2022-04-12 10:37:40 +0200

    overlays: remove vim-plugins-overrides

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,4 @@
 {
-  null-ls-update = import ./vim-plugins-overrides;
-
   sabnzbd-fix-missing-packages = import ./sabnzbd-fix-missing-dependencies;
 
   transgui-fix-duplicate-status = import ./transgui-fix-duplicate-status;
