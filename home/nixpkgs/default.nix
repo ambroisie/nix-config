b80053d7868f71d7fc28c9aa0ff71ef92a44c4b9
Bruno BELANYI <bruno@belanyi.fr>
2023-05-17 19:20:08 +0100

    home: add nixpkgs

---
+++ home/nixpkgs/default.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.my.home.nixpkgs;
+in
+{
+  options.my.home.nixpkgs = with lib; {
+    enable = mkEnableOption "nixpkgs configuration";
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      nixpkgs-review
+    ];
+
+    home.sessionVariables = {
+      GITHUB_TOKEN = ''$(cat "${config.age.secrets."github/token".path}")'';
+    };
+  };
+}
