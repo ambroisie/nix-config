3a7f4d126d7fc0e04e04881f1078af791aa58fc0
Bruno BELANYI <bruno@belanyi.fr>
2023-12-07 14:40:15 +0000

    hosts: homes: mousqueton: migrate 'git.package'

--- hosts/homes/ambroisie@mousqueton/default.nix
+++ hosts/homes/ambroisie@mousqueton/default.nix
@@ -4,11 +4,13 @@
   # Google specific configuration
   home.homeDirectory = "/usr/local/google/home/ambroisie";
 
-  programs.git.package = lib.mkForce pkgs.emptyDirectory;
-
   services.gpg-agent.enable = lib.mkForce false;
 
   my.home = {
+    git = {
+      package = pkgs.emptyDirectory;
+    };
+
     tmux = {
       # I use scripts that use the passthrough sequence often on this host
       enablePassthrough = true;
