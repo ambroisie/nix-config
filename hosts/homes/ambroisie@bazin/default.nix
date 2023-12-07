8398c4350a70ae08b5614b6cf6737c11471b609f
Bruno BELANYI <bruno@belanyi.fr>
2023-12-07 14:26:36 +0000

    hosts: homes: add work laptop

---
+++ hosts/homes/ambroisie@bazin/default.nix
@@ -0,0 +1,17 @@
+# Google Cloudtop configuration
+{ lib, pkgs, ... }:
+{
+  programs.git.package = lib.mkForce pkgs.emptyDirectory;
+
+  services.gpg-agent.enable = lib.mkForce false;
+
+  my.home = {
+    tmux = {
+      # I use scripts that use the passthrough sequence often on this host
+      enablePassthrough = true;
+
+      # HTerm uses `xterm-256color` as its `$TERM`, so use that here
+      trueColorTerminals = [ "xterm-256color" ];
+    };
+  };
+}
