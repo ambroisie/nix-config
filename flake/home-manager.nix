5d706dd2dce548e66d49b88fe452075978b6df38
Bruno BELANYI <bruno@belanyi.fr>
2024-01-03 16:50:16 +0000

    flake: home-manager: import common modules

--- flake/home-manager.nix
+++ flake/home-manager.nix
@@ -19,6 +19,8 @@ let
       # Enable home-manager
       programs.home-manager.enable = true;
     }
+    # Import common modules
+    "${self}/modules/common"
   ];
 
   mkHome = name: system: inputs.home-manager.lib.homeManagerConfiguration {
