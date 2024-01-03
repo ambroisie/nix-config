d181f3a71903c5bb5a8b0f89c3fe84023492e76e
Bruno BELANYI <bruno@belanyi.fr>
2024-01-03 16:51:34 +0000

    common: profiles: forward profiles to home-manager
    We can only do this now that every profile has been migrated, otherwise
    we would get errors about undeclared modules... It's not perfect, but
    it's good enough.

--- modules/common/profiles/default.nix
+++ modules/common/profiles/default.nix
@@ -1,5 +1,5 @@
 # Configuration that spans across system and home, or are almagations of modules
-{ ... }:
+{ config, lib, _class, ... }:
 {
   imports = [
     ./bluetooth
@@ -8,5 +8,18 @@
     ./laptop
     ./wm
     ./x
+  ];
+
+  config = lib.mkMerge [
+    # Transparently enable home-manager profiles as well
+    (lib.optionalAttrs (_class != "homeManager") {
+      home-manager.users.${config.my.user.name} = {
+        config = {
+          my = {
+            inherit (config.my) profiles;
+          };
+        };
+      };
+    })
   ];
 }
