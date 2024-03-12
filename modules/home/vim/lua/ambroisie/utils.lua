56e158f5c52d4333e7c1a3ddc3e2a34dc0aa40ae
Bruno BELANYI <bruno@belanyi.fr>
2024-03-12 12:30:36 +0000

    home: vim: lua: utils: fix deprecated function

--- modules/home/vim/lua/ambroisie/utils.lua
+++ modules/home/vim/lua/ambroisie/utils.lua
@@ -44,7 +44,7 @@ end
 --- @param bufnr int? buffer number
 --- @return table all active LSP client names
 M.list_lsp_clients = function(bufnr)
-    local clients = vim.lsp.buf_get_clients(bufnr)
+    local clients = vim.lsp.get_active_clients({ bufnr = bufnr or 0 })
     local names = {}
 
     for _, client in ipairs(clients) do
