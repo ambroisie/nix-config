452399ee4c036663e2aef0a802120445591931a6
Bruno BELANYI <bruno@belanyi.fr>
2024-07-10 22:16:51 +0100

    home: vim: oil: add detail view toggle mapping

--- modules/home/vim/plugin/settings/oil.lua
+++ modules/home/vim/plugin/settings/oil.lua
@@ -1,6 +1,8 @@
 local oil = require("oil")
 local wk = require("which-key")
 
+local detail = false
+
 oil.setup({
     view_options = {
         -- Show files and directories that start with "." by default
@@ -9,6 +11,19 @@ oil.setup({
         is_always_hidden = function(name, bufnr)
             return name == ".."
         end,
+    },
+    keymaps = {
+        ["gd"] = {
+            desc = "Toggle file detail view",
+            callback = function()
+                detail = not detail
+                if detail then
+                    oil.set_columns({ "icon", "permissions", "size", "mtime" })
+                else
+                    oil.set_columns({ "icon" })
+                end
+            end,
+        },
     },
 })
 
