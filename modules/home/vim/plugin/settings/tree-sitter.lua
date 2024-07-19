3438290e32adb1dfcc3e69b86f83effa279a7c27
Bruno BELANYI <bruno@belanyi.fr>
2024-07-19 11:13:11 +0000

    home: vim: tree-sitter: move mappings from 'after'

--- modules/home/vim/plugin/settings/tree-sitter.lua
+++ modules/home/vim/plugin/settings/tree-sitter.lua
@@ -1,4 +1,6 @@
 local ts_config = require("nvim-treesitter.configs")
+local wk = require("which-key")
+
 ts_config.setup({
     highlight = {
         enable = true,
@@ -51,3 +53,32 @@ ts_config.setup({
         },
     },
 })
+
+local motions = {
+    ["]m"] = "Next method start",
+    ["]M"] = "Next method end",
+    ["]S"] = "Next statement start",
+    ["]]"] = "Next class start",
+    ["]["] = "Next class end",
+    ["[m"] = "Previous method start",
+    ["[M"] = "Previous method end",
+    ["[S"] = "Previous statement start",
+    ["[["] = "Previous class start",
+    ["[]"] = "Previous class end",
+}
+
+local objects = {
+    ["aa"] = "a parameter",
+    ["ia"] = "inner parameter",
+    ["ab"] = "a block",
+    ["ib"] = "inner block",
+    ["ac"] = "a class",
+    ["ic"] = "inner class",
+    ["af"] = "a function",
+    ["if"] = "inner function",
+    ["ak"] = "a comment",
+    ["aS"] = "a statement",
+}
+
+wk.register(motions, { mode = "n" })
+wk.register(objects, { mode = "o" })
