950cf4dd059e74d87084747b25e70138753b82d5
Bruno BELANYI <bruno@belanyi.fr>
2025-03-31 11:27:18 +0000

    nixos: services: servarr: migrate jackett

--- modules/nixos/services/indexers/default.nix
+++ modules/nixos/services/indexers/default.nix
@@ -3,36 +3,14 @@
 let
   cfg = config.my.services.indexers;
 
-  jackettPort = 9117;
   nzbhydraPort = 5076;
 in
 {
   options.my.services.indexers = with lib; {
-    jackett.enable = mkEnableOption "Jackett torrent meta-indexer";
     nzbhydra.enable = mkEnableOption "NZBHydra2 usenet meta-indexer";
   };
 
   config = lib.mkMerge [
-    (lib.mkIf cfg.jackett.enable {
-      services.jackett = {
-        enable = true;
-      };
-
-      # Jackett wants to eat *all* my RAM if left to its own devices
-      systemd.services.jackett = {
-        serviceConfig = {
-          MemoryHigh = "15%";
-          MemoryMax = "25%";
-        };
-      };
-
-      my.services.nginx.virtualHosts = {
-        jackett = {
-          port = jackettPort;
-        };
-      };
-    })
-
     (lib.mkIf cfg.nzbhydra.enable {
       services.nzbhydra2 = {
         enable = true;
