ab34234c8392130d0158940d761dfa2923d527bf
Bruno BELANYI <bruno@belanyi.fr>
2026-01-26 16:57:25 +0000

    home: vim: lua: remove 'is_executable_condition'

--- modules/home/vim/lua/ambroisie/utils.lua
+++ modules/home/vim/lua/ambroisie/utils.lua
@@ -7,15 +7,6 @@ M.is_executable = function(cmd)
     return cmd and vim.fn.executable(cmd) == 1
 end
 
---- return a function that checks if a given command is executable
---- @param cmd string? command to check
---- @return fun(): boolean executable
-M.is_executable_condition = function(cmd)
-    return function()
-        return M.is_executable(cmd)
-    end
-end
-
 --- whether or not we are currently in an SSH connection
 --- @return boolean ssh connection
 M.is_ssh = function()
